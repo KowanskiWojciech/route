4df55b1af0e87e416adb2e2bc5c15b037bfce895
Wojciech Kowański <kowanski.wojciech@gmail.com>
2020-05-12 21:34:20 +0200

    all changes from begining

---
+++ src/components/Article.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Article = (props) => {
+  const styles = {
+    marginTop: 40,
+  };
+
+  return (
+    <article style={styles}>
+      <h3
+        style={{
+          marginBottom: 3,
+          textTransform: "uppercase",
+        }}
+      >
+        {props.title}
+      </h3>
+      <span
+        style={{
+          display: "block",
+          marginBottom: 10,
+          fontSize: 15,
+        }}
+      >
+        {props.author}
+      </span>
+      <p>{props.text}</p>
+    </article>
+  );
+};
+
+export default Article;
